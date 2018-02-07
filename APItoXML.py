c9c3dcda7e689bc9a56d78fe1ab44d5296ac03f5
Recall703 <monozuteoh0127@gmail.com>
2018-02-07 22:59:10 +0900

    Update APItoXML.py

--- APItoXML.py
+++ APItoXML.py
@@ -1,12 +1,14 @@
 from urllib.request import urlopen
 #from yahoo_quote_download import yqd
+import xmltodict
 import json
+from src.json2xml import Json2xml
 import dicttoxml
 from xml.dom.minidom import parseString
 
 #this function will obtain the api_url and get the data(in JSON file)
 #JSON in string will be returned.
-def getapidata(api_url):
+def getapidata(options):
     #API token = "J3Q2DC5XWYELA6G5"
     #url = api_url
     symbol = options[0]
@@ -18,26 +20,27 @@ def getapidata(api_url):
     content = json.loads(data)
     return content
     # print(content)
-    
-   
-#This function will convert the JSON data into XML file
-#Returned data type is string
-#The data is XML in string.
-def json2xml(json_data):
-    xml_content = dicttoxml.dicttoxml(json_data)
-    xml_content = bytes.decode(xml_content)
-    xml_content = parseString(xml_content)
-    xml_data = xml_content.toprettyxml()
-    return xml_data
 
-#This function will open file and write the XML code into the file
+#This function will convert the JSON data into XML file
+#Data type is string
+def json2xml(json_data):
+    #took others libraries to help:
+    #Library link:  https://github.com/vinitkumar/json2xml
+    data_object = Json2xml(json_data)
+    xml_output = data_object.json2xml()
+    return xml_output
+
+#This functino will write the data into xml file
+#input parameter is a string
+#output is a xml file, which named example.xml for this case
 def writexmltofile(xml_data):
     filename = "example.xml"
     fileobj = open(filename, "w")
+    fileobj.write("<?xml version=\"1.0\"?>\n")
     fileobj.write(xml_data)
     fileobj.close()
 
-#Code run here:
-data = getapidata("test") #in proper case, please replace the test to your API_link, and comment out line 11
-test = json2xml(data)
-writexmltofile(test)
+def start(options):
+    data = getapidata(options)
+    test = json2xml(data)
+    writexmltofile(test)
