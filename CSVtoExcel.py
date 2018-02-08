364d1433b2151de59e718794b39773532249782a
Recall703 <monozuteoh0127@gmail.com>
2018-02-08 13:15:34 +0800

    Update CSVtoExcel.py

--- CSVtoExcel.py
+++ CSVtoExcel.py
@@ -4,20 +4,22 @@ import xlsxwriter
 
 # if we read f.csv we will write f.xlsx
 
-#Insert your csv file name here
-csvfilename = "ResidentData.csv"
 
-#change csv file to xlsx file
+def start():
+	#Insert your csv file name here
+	csvfilename = "example.csv"
+
+	#change csv file to xlsx file
 
 
-wb = xlsxwriter.Workbook(csvfilename.replace(".csv",".xlsx"))
-ws = wb.add_worksheet("WS1")    # your worksheet title here
-with open(csvfilename,'r') as csvfile:
-    table = csv.reader(csvfile)
-    i = 0
-    # write each row from the csv file as text into the excel file
-    # this may be adjusted to use 'excel types' explicitly (see xlsxwriter doc)
-    for row in table:
-        ws.write_row(i, 0, row)
-        i += 1
-wb.close()
+	wb = xlsxwriter.Workbook(csvfilename.replace(".csv",".xlsx"))
+	ws = wb.add_worksheet("WS1")    # your worksheet title here
+	with open(csvfilename,'r') as csvfile:
+	    table = csv.reader(csvfile)
+	    i = 0
+	    # write each row from the csv file as text into the excel file
+	    # this may be adjusted to use 'excel types' explicitly (see xlsxwriter doc)
+	    for row in table:
+	        ws.write_row(i, 0, row)
+	        i += 1
+	wb.close()
